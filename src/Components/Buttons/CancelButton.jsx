734dcdf8aa124703d46685cb25f862ed084078df
aangee <gohanss4@hotmail.fr>
2023-01-07 17:31:34 +0100

    feat: add button component (#36)
    * feat: add button component
    
    * fix: link href

---
+++ src/Components/Buttons/CancelButton.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const CancelButton = () => {
+    return (
+         <div className="cancel-button">
+            <a className="cancel-button-text" href="/">
+                Retour
+            </a>
+        </div>
+    )
+}
+
+export default CancelButton;
---
+++ src/Components/Buttons/LinkButton.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const LinkButton = () => {
+    return (
+        <div className="link-button">
+            <a className="link-button-text" href="/">
+                Link
+            </a>
+        </div>
+    )
+}
+
+export default LinkButton;
