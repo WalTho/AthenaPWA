41a132da738bd47a5c957549bed0ba4143bf7a1e
Fabio <sperini.fab@gmail.com>
2023-01-10 19:26:06 +0100

    fix: remove unused props

--- src/Components/Buttons/ValidButton.jsx
+++ src/Components/Buttons/ValidButton.jsx
@@ -2,14 +2,13 @@ import React from "react";
 import uuid from 'react-uuid';
 
 const ValidButton = (props) => {
-    const {Label, Link} = props;
     const unique_id = uuid();
      return(
         <div className="valid-button" key={unique_id}>
             <a 
                 className="valid-button-text" 
                 key={unique_id} 
-                href={props?.Link ?? '/'}
+                href={props.Link ??= '/'}
             >
                {props?.Label}
             </a>
