606c9b196769ebafb90ffc952af06932a8547b27
Thomas F <waltorias.as@gmail.com>
2023-03-12 21:53:09 +0100

    fix: scss optimization

--- src/Components/Maps/Centauri/CentauriMap.jsx
+++ src/Components/Maps/Centauri/CentauriMap.jsx
@@ -23,7 +23,7 @@ const CentauriMap = () => {
 
                     <g className="maps-svg-avernus">
                         <circle cx="678" cy="685" r="16" fill="#776239"
-                                cursor="pointer" className="maps-svg-avernus-planet"
+                                cursor="pointer" className="maps-svg-avernus-planet highlight"
                                 onClick={() => openModal(
                                     <ModalCard
                                         onClose={closeModal}
@@ -36,7 +36,7 @@ const CentauriMap = () => {
                     </g>
                     <g className="maps-svg-triton">
                         <circle cx="288" cy="669" r="16" fill="#7BA5AB"
-                                cursor="pointer" className="maps-svg-triton-planet"
+                                cursor="pointer" className="maps-svg-triton-planet highlight"
                                 onClick={() => openModal(
                                     <ModalCard
                                         onClose={closeModal}
@@ -49,7 +49,7 @@ const CentauriMap = () => {
                     </g>
                     <g className="maps-svg-exolia">
                         <circle cx="213" cy="937" r="16" fill="#2C7652"
-                                cursor="pointer" className="maps-svg-exolia-planet"
+                                cursor="pointer" className="maps-svg-exolia-planet highlight"
                                 onClick={() => openModal(
                                     <ModalCard
                                         onClose={closeModal}
@@ -62,7 +62,7 @@ const CentauriMap = () => {
                     </g>
                     <g className="maps-svg-pertam">
                         <circle cx="216" cy="538" r="16" fill="#D3AF67"
-                                cursor="pointer" className="maps-svg-pertam-planet"
+                                cursor="pointer" className="maps-svg-pertam-planet highlight"
                                 onClick={() => openModal(
                                     <ModalCard
                                         onClose={closeModal}
@@ -75,7 +75,7 @@ const CentauriMap = () => {
                     </g>
                     <g className="maps-svg-treossa">
                         <circle cx="785" cy="538" r="16" fill="#753A3A"
-                                cursor="pointer" className="maps-svg-treossa-planet"
+                                cursor="pointer" className="maps-svg-treossa-planet highlight"
                                 onClick={() => openModal(
                                     <ModalCard
                                         onClose={closeModal}
@@ -88,7 +88,7 @@ const CentauriMap = () => {
                     </g>
                     <g className="maps-svg-alien">
                         <circle cx="857" cy="202" r="16" fill="#654E82"
-                                cursor="pointer" className="maps-svg-alien-planet"
+                                cursor="pointer" className="maps-svg-alien-planet highlight"
                                 onClick={() => openModal(
                                     <ModalCard
                                         onClose={closeModal}
@@ -101,7 +101,7 @@ const CentauriMap = () => {
                     </g>
                     <g className="maps-svg-mars">
                         <circle cx="440" cy="139" r="16" fill="#A76042"
-                                cursor="pointer" className="maps-svg-mars-planet"
+                                cursor="pointer" className="maps-svg-mars-planet highlight"
                                 onClick={() => openModal(
                                     <ModalCard
                                         onClose={closeModal}
@@ -114,7 +114,7 @@ const CentauriMap = () => {
                     </g>
                     <g className="maps-svg-terra">
                         <circle cx="337" cy="240" r="16" fill="#579B6E"
-                                cursor="pointer" className="maps-svg-terra-planet"
+                                cursor="pointer" className="maps-svg-terra-planet highlight"
                                 onClick={() => openModal(
                                     <ModalCard
                                         onClose={closeModal}
@@ -127,7 +127,7 @@ const CentauriMap = () => {
                     </g>
                     <g className="maps-svg-titan">
                         <circle cx="838" cy="177" r="8" fill="#D09259"
-                                cursor="pointer" className="maps-svg-titan-planet"
+                                cursor="pointer" className="maps-svg-titan-planet highlight"
                                 onClick={() => openModal(
                                     <ModalCard
                                         onClose={closeModal}
@@ -140,7 +140,7 @@ const CentauriMap = () => {
                     </g>
                     <g className="maps-svg-europa">
                         <circle cx="432" cy="110" r="8" fill="#64998C"
-                                cursor="pointer" className="maps-svg-europa-planet"
+                                cursor="pointer" className="maps-svg-europa-planet highlight"
                                 onClick={() => openModal(
                                     <ModalCard
                                         onClose={closeModal}
@@ -153,7 +153,7 @@ const CentauriMap = () => {
                     </g>
                     <g className="maps-svg-luna">
                         <circle cx="319" cy="212" r="8" fill="#828282"
-                                cursor="pointer" className="maps-svg-luna-planet"
+                                cursor="pointer" className="maps-svg-luna-planet highlight"
                                 onClick={() => openModal(
                                     <ModalCard
                                         onClose={closeModal}
@@ -166,7 +166,7 @@ const CentauriMap = () => {
                     </g>
                     <g className="maps-svg-rothis">
                         <circle cx="189" cy="916" r="8" fill="#636894"
-                                cursor="pointer" className="maps-svg-rothis-planet"
+                                cursor="pointer" className="maps-svg-rothis-planet highlight"
                                 onClick={() => openModal(
                                     <ModalCard
                                         onClose={closeModal}
@@ -180,7 +180,7 @@ const CentauriMap = () => {
                     <g className="maps-svg-overvent">
                         <circle cx="540" cy="538" r="109.5" fill="#62452A"
                                 stroke="url(#paint6_radial_135_48)" strokeWidth="30"
-                                cursor="pointer" className="maps-svg-overvent-planet"
+                                cursor="pointer" className="maps-svg-overvent-planet highlight"
                                 onClick={() => openModal(
                                     <ModalCard
                                         onClose={closeModal}
@@ -193,7 +193,7 @@ const CentauriMap = () => {
                     </g>
                     <path
                         d="M410.433 480.75L410 480L409.567 480.75L401.773 494.25L401.34 495H402.206H417.794H418.66L418.227 494.25L410.433 480.75Z"
-                        fill="#992B95" stroke="black" cursor="pointer" className="maps-svg-wormhole-planet"
+                        fill="#992B95" stroke="black" cursor="pointer" className="maps-svg-wormhole-planet highlight"
                         onClick={() => openModal(
                             <ModalCard
                                 onClose={closeModal}
